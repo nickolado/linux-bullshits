c4304994585cc147de9afb62201e0c822827972a
Daniel <daniel.richarddosanjossilva@gmail.com>
2024-09-25 13:45:41 -0300

    adicionado echo

--- instalacao.sh
+++ instalacao.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
+# Inicio
+echo "Iniciando script"
+
 # Corrigir o fuso horário
+echo "Ajustando fuso horário"
 timedatectl set-timezone America/Bahia
+sleep 2
 
 # Criar pasta tivic na raiz
+echo "Criando pasta tivic"
 cd /
 mkdir tivic
 sleep 2
 
 # Atualizar os rep
+echo "Atualizando SO"
 apt update
 sleep 5
 apt upgrade
 sleep 5
 
 # Instalação do Java
-
+echo "Instalando Java"
 apt install default-jre
 sleep 5 
 apt install openjdk-8-jre-headless
 sleep 5 
 apt install default-jdk
-sleep 5
+sleep 2
 
 # Instalação do Tomcat
-
+echo "Instalando Tomcat"
 wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz
 sleep 5
 mkdir /opt/tomcat
@@ -40,13 +47,17 @@ cd /opt/tomcat/bin
 sleep 2
 
 # Instalação do PostgreSQL 14
-
+echo "Instalando Postgresql"
 apt install postgresql-14
+sleep 2
 
 # Aplicar configuração no Postgres
-
+echo "Configurando Postgres"
 sed -i "s/^local\s*all\s*postgres\s*peer/local   all   postgres   trust/" /etc/postgresql/14/main/pg_hba.conf
 sleep 2
 echo "host    all             all             192.168.1.0/24          md5" >> /etc/postgresql/14/main/pg_hba.conf
 echo "password_encryption = md5" >> /etc/postgresql/14/main/postgresql.conf
 echo "listen_addresses = '*'" >> /etc/postgresql/14/main/postgresql.conf
+
+# Fim do script
+echo "Final do script"
